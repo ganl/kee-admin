02f02240cc77946a57d2d939f5c6a11ae6e60c06
ganl <769323213@qq.com>
2017-08-18 17:08:16 +0800

    add Element-ui and test component

--- src/main.js
+++ src/main.js
@@ -2,9 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import { Button, Select, TimePicker, DatePicker } from 'element-ui'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+/* 或写为
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
+
+Vue.use(TimePicker)
+Vue.use(DatePicker)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Test from '@/views/Test'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/test',
+      name: 'Test',
+      component: Test
     }
   ]
 })
